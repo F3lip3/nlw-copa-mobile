eca49adff9cea21f0a8c27443e5061afd92b27dd
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2022-11-02 23:30:20 -0300

    feat: add find pool by code screen

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { Loading } from './src/components/Loading';
 import { AuthContextProvider } from './src/contexts/AuthContext';
-import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
@@ -25,7 +25,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <New /> : <Loading />}
+        {fontsLoaded ? <Find /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
